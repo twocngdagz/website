24dd9b34aa373a8120e5543745af273e94a49233
Dayle Rees <thepunkfan@gmail.com>
2013-03-16 20:11:41 +0000

    Replicate config for production.

---
+++ app/config/website.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Content Directory
+    |--------------------------------------------------------------------------
+    |
+    | The directory in which the website will search for its markdown content
+    | files. Article files must use the .md extension.
+    |
+    */
+
+    'content' => __DIR__.'/../../content',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Article Cache Duration
+    |--------------------------------------------------------------------------
+    |
+    | How long to cache the entire Article collection before regenerating the
+    | index in minutes.
+    |
+    */
+
+    'cache' => 1,
+);
